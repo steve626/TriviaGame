4a190fcc340515d64005eb771459dfccd2046349
Steve Valasek <svalasek@gmail.com>
2018-02-27 10:35:55 -0700

    basic layout and psuedocode set out

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+// warbler quiz
+
+//make functions for gameplay
+
+// make const for questions and answers
+
+// countdown timer, 20 seconds per question 
+
+//correct answer add to ones right, change font to green switch after 4 seconds?
+
+//incorrect answer add to ones wrong, chanage font to red switch after 4 seconds?
+
+//score percentage correct in end screen. 
+
+//restart button?
+
+//reset f()
